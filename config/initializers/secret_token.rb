c807bc51a3211bf8205a9e729c608f259aa0ce0c
Deane Lawrence <deaneslawrence@gmail.com>
2014-01-18 21:36:55 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Evespace::Application.config.secret_key_base = 'a5687f3c4e9598783d4bf8bd6c923b50cf34507afe9273299d490b22cc6734f497cc8859650acdee509d604cdcf20589b906a455cbafce2700e393e09b92826f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Evespace::Application.config.session_store :cookie_store, key: '_evespace_session'
